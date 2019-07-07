fbd3bd75057ef14641b342812d9b6fda3c6cb827
Matteo Crippa <matteo.crippa@gmail.com>
2019-07-07 18:13:50 +0200

    Fix blaming for #1, pr should be a better proactive way to be part of a community

--- app/src/main/java/matteocrippa/it/karamba/Bitmap+Extension.kt
+++ app/src/main/java/matteocrippa/it/karamba/Bitmap+Extension.kt
@@ -1,6 +1,7 @@
 package matteocrippa.it.karamba
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Matrix
 import android.util.Base64
 import java.io.ByteArrayOutputStream
@@ -17,13 +18,5 @@ fun Bitmap.base64(): String {
 }
 
 fun Bitmap.resize(newWidth: Int, newHeight: Int): Bitmap {
-    val width = this.width
-    val height = this.height
-    val scaleWidth = newWidth.toFloat() / width
-    val scaleHeight = newHeight.toFloat() / height
-    val matrix = Matrix()
-    matrix.postScale(scaleWidth, scaleHeight)
-    val resizedBitmap = Bitmap.createScaledBitmap(this, width, height, false)
-    resizedBitmap.recycle()
-    return resizedBitmap
+    return Bitmap.createScaledBitmap(this, newWidth, newHeight, false)
 }
