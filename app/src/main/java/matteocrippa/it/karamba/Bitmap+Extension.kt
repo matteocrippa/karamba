7a42b2c68f98aef511f95cb7c501e7ee9d2745e8
Mahmoud <ma.shahoud@gmail.com>
2019-07-07 19:53:38 +0300

    added Bitmap.overlay

--- app/src/main/java/matteocrippa/it/karamba/Bitmap+Extension.kt
+++ app/src/main/java/matteocrippa/it/karamba/Bitmap+Extension.kt
@@ -20,3 +20,17 @@ fun Bitmap.base64(): String {
 fun Bitmap.resize(newWidth: Int, newHeight: Int): Bitmap {
     return Bitmap.createScaledBitmap(this, newWidth, newHeight, false)
 }
+
+/**
+ * Overlay a bitmap over another one.
+ *
+ * @param overlay The overlay bitmap.
+ * @return A bitmap which contains the overlay over the original bitmap.
+ */
+fun Bitmap.overlay(overlay: Bitmap): Bitmap {
+    val result = Bitmap.createBitmap(width, height, config)
+    val canvas = Canvas(result)
+    canvas.drawBitmap(this, Matrix(), null)
+    canvas.drawBitmap(overlay, Matrix(), null)
+    return result
+}
